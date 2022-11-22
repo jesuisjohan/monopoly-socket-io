7d4264ade7de930aeee1a2e1cc9c9e87fb98e6a8
Mai Hoàng Anh Vũ <thomas.maihoanganhvu@gmail.com>
2022-11-22 23:05:28 +0700

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,12 @@
+// color array for players
+const COLORS = {
+  BLACK: "black",
+  WHITE: "white",
+  ORANGE: "orange",
+  RED: "red",
+  BLUE: "blue",
+  GREEN: "green",
+  YELLOW: "yellow",
+};
+
+export default COLORS;
