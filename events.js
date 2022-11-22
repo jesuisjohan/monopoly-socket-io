7bfc0e099939ba381c0e98fa31aaa0442b16cec0
Mai Hoàng Anh Vũ <thomas.maihoanganhvu@gmail.com>
2022-11-22 22:37:45 +0700

    Create events.js

---
+++ events.js
@@ -0,0 +1,19 @@
+const EVENTS = {
+  MAKE_MOVE: "makeMove",
+  NEW_PLAYER: "new player",
+  PLAYER_HAS_MOVED: "player has moved",
+  END_TURN: "end turn",
+  SEND_DICE: "send dice",
+  END_JAIL: "in jail",
+  BUY_PROPERTY: "buy property",
+  SEND_CHAT: "send chat",
+  PUT_ON_OPEN_MARKET: "put on open market",
+  MAKE_OFFER: "make offer",
+  ACCEPT_OFFER: "accept offer",
+  DECLINE_OFFER: "decline offer",
+  MAKE_SALE: "make sale",
+  START_GAME: "start game",
+  REMOVE_SALE: "remove sale",
+};
+
+export default EVENTS;
